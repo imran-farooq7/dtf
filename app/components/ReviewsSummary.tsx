647d5e817933b973815739620420438a9d938ebb
imran farooq <imran_farooq7@hotmail.com>
2023-10-03 04:24:58 +0500

    Reviews summary component completed

---
+++ app/components/ReviewsSummary.tsx
@@ -0,0 +1,58 @@
+const ReviewsSummary = () => {
+	return (
+		<div className="flex justify-between">
+			<div className="flex flex-col sm:flex-row gap-10">
+				<div className="space-y-2">
+					<div className="flex items-center gap-4">
+						<img src="/star.svg" alt="star" width="20px" />
+						<p className="text-zinc-900 text-3xl font-normal ">4.8</p>
+					</div>
+					<p className="text-zinc-900 text-base font-normal">174 Reviews</p>
+				</div>
+				<div>
+					<div className="flex items-center gap-4">
+						<img src="/stars.svg" alt="star" />
+						<img src="/progress 1.svg" alt="progress" />
+						<div className="text-zinc-900 text-base font-normal leading-relaxed">
+							(156)
+						</div>
+					</div>
+					<div className="flex items-center gap-4">
+						<img src="/stars.svg" alt="star" />
+						<img src="/progress 1.svg" alt="progress" />
+						<div className="text-zinc-900 text-base font-normal leading-relaxed">
+							(156)
+						</div>
+					</div>
+					<div className="flex items-center gap-4">
+						<img src="/stars.svg" alt="star" />
+						<img src="/progress 1.svg" alt="progress" />
+						<div className="text-zinc-900 text-base font-normal leading-relaxed">
+							(156)
+						</div>
+					</div>
+					<div className="flex items-center gap-4">
+						<img src="/stars.svg" alt="star" />
+						<img src="/progress 1.svg" alt="progress" />
+						<div className="text-zinc-900 text-base font-normal leading-relaxed">
+							(156)
+						</div>
+					</div>
+					<div className="flex items-center gap-4">
+						<img src="/stars.svg" alt="star" />
+						<img src="/progress 1.svg" alt="progress" />
+						<div className="text-zinc-900 text-base font-normal leading-relaxed">
+							(156)
+						</div>
+					</div>
+				</div>
+			</div>
+			<img
+				src="/filter.svg"
+				alt="filter"
+				className="hidden sm:block self-start"
+			/>
+		</div>
+	);
+};
+export default ReviewsSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Product from "./components/Product";
+import ReviewsSummary from "./components/ReviewsSummary";
 
 export default function Home() {
 	return (
 		<>
 			<Product />
+			<ReviewsSummary />
 		</>
 	);
 }
